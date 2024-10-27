441a5c0abba8fa6b14f1039bb035c74f69e4d077
Abujalala <abujalala@gmail.com>
2024-10-27 05:59:51 +0200

    ficed

--- src/components/Centers.vue
+++ src/components/Centers.vue
@@ -34,22 +34,23 @@ function ema(city, id) {
 <template>
 
 <div class="column">
+  <div class="q-px-md q-pb-md" style=" position: fixed; top: 0; left: 0; right: 0; z-index: 200; background-color: rgb(22, 6, 41);  max-width: 450px; margin: auto; border-bottom: 2px solid rgb(142, 134, 156); box-shadow: 1px 5px 5px black;">
   <transition appear enter-active-class="animated backInDown">
-    <h1 class="text-h4 q-mb-xs text-yellow-3 ">البلديــــــــات</h1>
+    <h1 class="text-h4 q-mb-xs q-mt-md text-yellow-3 ">البلديــــــــات</h1>
   </transition>
 
   <transition appear enter-active-class="animated slideInRight">
-    <div class="row q-mt-lg justify-between items-center full-width reverse  q-pa-sm q-pr-lg"
-    style="border-radius: 10px; background-color: rgba(192, 244, 163, 0.3);"
+    <div class="row q-mt-lg justify-between items-center  reverse  q-pa-sm q-pr-lg"
+    style="border-radius: 10px; background-color: rgba(214, 212, 217, 0.3);"
     >
       <p class="q-mb-xs text-h6">بحـــث</p>
       <q-input class="bg-white text-h6 q-px-md " style="width: 80%; border-radius: 20px;" rounded v-model="search"
         dense input-class="text-right" />
     </div>
   </transition>
-
+</div>
   <transition appear enter-active-class="animated slideInLeft">
-    <q-list class="q-my-sm full-width" style="border-radius: 10px; margin-bottom: auto;">
+    <q-list class="q-my-sm full-width" style="border-radius: 10px; margin-bottom: auto; margin-top: 170px;">
       <q-item clickable v-ripple class=" q-pa-sm q-my-sm text-h4 text-white " v-for="n in uniqueCitiesfiltered" :key="n.id"
         @click="ema(n.city, n.cityid)"
         style="box-shadow: 1px 1px 2px rgb(1, 17, 33); border-radius: 10px; background-color: rgba(162, 184, 228, 0.3);  border-bottom: 1px solid rgb(246, 247, 246);">
